7c36e9ca6a311272ee3cbb8ba24b32eece310767
ediazjz <iam.ediazjz@gmail.com>
2025-01-30 05:45:14 -0600

    build: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		}),
+		tailwindcss()
+	],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
