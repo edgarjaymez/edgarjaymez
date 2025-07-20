921b9c5036cdacbb2135433329795f82f0edd1ee
edgarjaymez <iam.ediazjz@gmail.com>
2025-07-19 19:20:16 -0600

    build: remove unused libraries and boilerplate code

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		paraglide({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		}),
-		tailwindcss()
-	],
+	plugins: [sveltekit(), tailwindcss()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
