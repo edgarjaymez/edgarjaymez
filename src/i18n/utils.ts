f6c3bfbe48b4710f1e028117776778524fe3ca08
ediazjz <iam.ediazjz@gmail.com>
2026-03-29 13:54:10 -0600

    fix: fix TS errors with assertions

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,5 @@
-import { ui, defaultLang, showDefaultLang } from './ui';
+import { ui, defaultLang, showDefaultLang, routes } from './ui';
+import { getKeyByValue } from '../utils';
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
@@ -8,12 +9,18 @@ export function getLangFromUrl(url: URL) {
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof typeof ui[typeof defaultLang]) {
+    // TODO: Refactor to remove assertion
     return (ui[lang] as Record<string, string>)[key] || ui[defaultLang][key];
   }
 }
 
 export function useTranslatedPath(lang: keyof typeof ui) {
-  return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
+  return function translatePath(path: string, l: keyof typeof routes = lang) {
+    const pathName = path.replaceAll('/', '')
+    // TODO: Refactor to remove assertion
+    const hasTranslation = defaultLang !== l && routes[l] !== undefined && routes[l][pathName as keyof typeof routes[typeof lang]] !== undefined
+    const translatedPath = hasTranslation ? '/' + routes[l][pathName as keyof typeof routes[typeof lang]] : path
+
+    return !showDefaultLang && l === defaultLang ? translatedPath : `/${l}${translatedPath}`
   }
 }
