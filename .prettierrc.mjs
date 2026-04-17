27dd188bc3006a2ebea39d4832e7b9a01f6a1b6a
Edgar Diaz <ediazjz@Edgars-MacBook-Air.local>
2026-04-16 21:57:30 -0600

    build: add Prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro, *.svelte",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
