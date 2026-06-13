a12d30d41b755a2b6149cc18283b1591c4a40e10
edgarjaymez <iam.ediazjz@gmail.com>
2026-06-13 08:11:24 -0600

    build: cloudfare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import markdoc from "@astrojs/markdoc";
 // https://astro.build/config
 export default defineConfig({
   site: "https://edgarjaymez.com",
-  integrations: [lit(), sitemap(), markdoc()],
+  integrations: [sitemap(), markdoc()],
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
