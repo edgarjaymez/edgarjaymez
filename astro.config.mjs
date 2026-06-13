80234d39f7f255fa0115a2b629e26705cc9f77ff
edgarjaymez <iam.ediazjz@gmail.com>
2026-06-13 06:13:29 -0600

    build: cloudfare testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://edgarjaymez.vercel.app",
+  site: "https://edgarjaymez.com",
   integrations: [lit(), sitemap(), markdoc()],
   i18n: {
     locales: ["en", "es"],
